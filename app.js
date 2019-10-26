bfdd9ce4ec66ba997386e82a82c1806e3c2040d2
julian <julian.quinteror1@gmail.com>
2019-10-25 21:59:26 -0500

    Added slider

---
+++ app.js
@@ -0,0 +1,4 @@
+const carousel = document.querySelector("[data-target=`carousel`]");
+const card = document.querySelector("[data-target=`card`]");
+const leftButton = document.querySelector("[data-target=`slideLeft`]");
+const rightButton = document.querySelector("[data-target=`slideRight`]");
