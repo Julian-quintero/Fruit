9365a15edc2923f7a9f39455ee43746e97bf49ce
julian <julian.quinteror1@gmail.com>
2019-10-29 23:37:35 -0500

    button animations added

--- app.js
+++ app.js
@@ -1,179 +1,173 @@
-document.addEventListener("DOMContentLoaded", () =>{
+document.addEventListener("DOMContentLoaded", () => {
 
     var continuousElements = document.getElementsByClassName('trackTitle-right');
-    var viewed;
     for (var i = 0; i < continuousElements.length; i++) {
-     var con = new Waypoint({
-        element: continuousElements[i],
-        handler: function(direction) {
-    
-    if (direction=="down") {
-        anime({
-            targets: ".trackTitle-right",
-            easing: "easeOutExpo",
-            opacity:[0,1],
-            duration:5000,
-            translateY:[0,20],
+        var con = new Waypoint({
+            element: continuousElements[i],
+            handler: function (direction) {
 
-           })
-    
-        viewed = 1;
-    
-    }        
-    
-    
-    
-        },
-    
-        offset:"100%"
-      })
-    
-     if (viewed==1) {
-        con.disable();
-    
-         
-     }
-    
+                if (direction == "down") {
+                    anime({
+                        targets: ".trackTitle-right",
+                        easing: "easeOutExpo",
+                        opacity: [0, 1],
+                        duration: 5000,
+                        translateY: [0, 20],
+
+                    })
+
+
+                    this.disable();
+                }
+
+
+
+            },
+
+            offset: "100%"
+        })
+
+        
+
+
+        
+
     }
 
     //
 
     var continuousElements = document.getElementsByClassName('trackTitle-left');
-    var viewed;
     for (var i = 0; i < continuousElements.length; i++) {
-     var con2 = new Waypoint({
-        element: continuousElements[i],
-        handler: function(direction) {
-    
-    if (direction=="down") {
-        anime({
-            targets: ".trackTitle-left",
-            easing: "easeOutExpo",
-            opacity:[0,1],
-            duration:5000,
-            translateY:[0,20],
+        var con2 = new Waypoint({
+            element: continuousElements[i],
+            handler: function (direction) {
 
-           })
-    
-        viewed = 1;
-    
-    }        
-    
-    
-    
-        },
-    
-        offset:"100%"
-      })
-    
-     if (viewed==1) {
-enabled:false;    
-         
-     }
-    
+                if (direction == "down") {
+                    anime({
+                        targets: ".trackTitle-left",
+                        easing: "easeOutExpo",
+                        opacity: [0, 1],
+                        duration: 5000,
+                        translateY: [0, 20],
+
+                    })
+
+                    this.disable();
+
+                }
+
+
+
+            },
+
+            offset: "100%"
+        })
+
+     
+
     }
 
     //
+    var con = [];
+    var continuousElements = document.querySelectorAll('.trackContent-right');
 
-    var continuousElements = document.getElementsByClassName('trackContent-right');
-    var viewed;
     for (var i = 0; i < continuousElements.length; i++) {
-     var con2 = new Waypoint({
-        element: continuousElements[i],
-        handler: function(direction) {
-    
-    if (direction=="down") {
-        anime({
-            targets: ".trackContent-right",
-            easing: "easeOutExpo",
-            opacity:[0,1],
-            duration:5000,
-            translateY:[20,0],
 
-           })
-    
-        viewed = 1;
-    
-    }        
-    
-    
-    
-        },
-    
-        offset:"95%"
-      })
-    
-     if (viewed==1) {
-        con2.disable();
-    
-         
-     }
-    
+        con[i] = new Waypoint({
+            element: continuousElements[i],
+            handler: function (direction) {
+
+               
+
+                if (direction == "down") {
+                    anime({
+                        targets: ".trackContent-right",
+                        easing: "easeOutExpo",
+                        opacity: [0, 1],
+                        duration: 5000,
+                        translateY: [20, 0],
+
+                    })
+                    this.disable();
+
+
+
+                }
+
+
+
+            },
+
+            offset: "95%"
+        })
+
+
+
     }
 
     //
 
     var continuousElements = document.getElementsByClassName('trackContent-left');
-    var viewed;
     for (var i = 0; i < continuousElements.length; i++) {
-     var con2 = new Waypoint({
-        element: continuousElements[i],
-        handler: function(direction) {
-    
-    if (direction=="down") {
-        anime({
-            targets: ".trackContent-left",
-            easing: "easeOutExpo",
-            opacity:[0,1],
-            duration:5000,
-            translateY:[20,0]
+        var con2 = new Waypoint({
+            element: continuousElements[i],
+            handler: function (direction) {
 
-           })
-    
-        viewed = 1;
-    
-    }        
-    
-    
-    
-        },
-    
-        offset:"95%"
-      })
-    
-     if (viewed==1) {
-        con2.disable();
-    
-         
-     }
-    
+                if (direction == "down") {
+                    anime({
+                        targets: ".trackContent-left",
+                        easing: "easeOutExpo",
+                        opacity: [0, 1],
+                        duration: 5000,
+                        translateY: [20, 0]
+
+                    })
+
+                    con2.disable();
+
+
+                }
+
+
+
+            },
+
+            offset: "95%"
+        })
+
+
+
+
+
+
     }
-    
+
     //animation 2
 
-    
-    var way = new Waypoint({
+
+    var textvideo = new Waypoint({
         element: document.querySelector('.text-video'),
-        handler: function(direction) {
-            
-            if (direction=="down") {
+        handler: function (direction) {
+
+            if (direction == "down") {
                 anime({
                     targets: ".text-video",
                     easing: "easeOutQuint",
-                    opacity:[0,1],
-                    duration:5000,
-                    translateX:[150,0],
+                    opacity: [0, 1],
+                    duration: 5000,
+                    translateX: [-150, 0],
 
-    
-                   
-                   
-                   })
-            }      
+
+
+
+                })
+                textvideo.disable();
+
+            }
         },
         offset: '100%'
-      })
-    
-      way.enable();
+    })
+
 
 
 })
@@ -182,48 +176,169 @@ enabled:false;
 
 
 
-var way = new Waypoint({
+var cardtesti = new Waypoint({
     element: document.querySelector('.card-content-testimony'),
-    handler: function(direction) {
-        
-        if (direction=="down") {
+    handler: function (direction) {
+
+        if (direction == "down") {
             anime({
                 targets: ".card-content-testimony",
                 easing: "easeOutExpo",
-                opacity:[0,1],
-                duration:5000,
-                translateY:[100,0]
+                opacity: [0, 1],
+                duration: 5000,
+                translateY: [100, 0]
 
 
-               
-               
-               })
-        }      
+
+
+            })
+            cardtesti.disable();
+        }
     },
     offset: '100%'
-  })
+})
 
 
-  var way = new Waypoint({
+var trackTitlep = new Waypoint({
     element: document.querySelector('.trackTitle-products'),
-    handler: function(direction) {
-        
-        if (direction=="down") {
+    handler: function (direction) {
+
+        if (direction == "down") {
             anime({
                 targets: ".trackTitle-products",
                 easing: "easeOutExpo",
-                opacity:[0,1],
-                duration:5000,
-                translateY:[100,0]
+                opacity: [0, 1],
+                duration: 5000,
+                translateY: [100, 0]
 
 
-               
-               
-               })
-        }      
+
+
+            })
+            trackTitlep.disable();
+        }
     },
     offset: '100%'
-  })
+})
+
+var cuadros1 = new Waypoint({
+    element: document.querySelector('#cuadros'),
+    handler: function (direction) {
+
+        if (direction == "down") {
+            anime({
+                targets: "#cuadros",
+                easing: "easeOutExpo",
+                opacity: [0, 1],
+                duration: 5000,
+                translateX:[-100, 0],
+                
+
+
+
+            })
+            cuadros1.disable();
+        }
+    },
+    offset: '100%'
+})
+
+var cuadros2 = new Waypoint({
+    element: document.querySelector('.cuadros2'),
+    handler: function (direction) {
+
+        if (direction == "down") {
+            anime({
+                targets: ".cuadros2",
+                easing: "easeOutExpo",
+                opacity: [0, 1],
+                duration: 5000,
+                height:["0vh","60vh"]
+
+
+
+
+            })
+            cuadros2.disable();
+
+        }
+    },
+    offset: '100%'
+})
+
+
+var continuousElements = document.getElementsByClassName('trackButton-par');
+    for (var i = 0; i < continuousElements.length; i++) {
+        var con2 = new Waypoint({
+            element: continuousElements[i],
+            handler: function (direction) {
+
+                if (direction == "down") {
+                    anime({
+                        targets: ".trackButton-par",
+                        easing: "easeOutExpo",
+                        opacity: [0, 1],
+                        duration: 5000,
+                        translateX: [-100, 0]
+
+                    })
+
+                    this.disable();
+
+
+                }
+
+
+
+            },
+
+            offset: "95%"
+        })
+
+
+
+
+
+
+    }
+
+    var continuousElements = document.getElementsByClassName('trackButton-impar');
+    for (var i = 0; i < continuousElements.length; i++) {
+        var con2 = new Waypoint({
+            element: continuousElements[i],
+            handler: function (direction) {
+
+                if (direction == "down") {
+                    anime({
+                        targets: ".trackButton-impar",
+                        easing: "easeOutExpo",
+                        opacity: [0, 1],
+                        duration: 5000,
+                        translateX: [-100, 0]
+
+                    })
+
+                    this.disable();
+
+
+                }
+
+
+
+            },
+
+            offset: "95%"
+        })
+
+
+
+
+
+
+    }
+
+
+
 
 
 
